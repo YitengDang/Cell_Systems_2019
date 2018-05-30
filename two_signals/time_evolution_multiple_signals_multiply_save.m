8bfa1a5e22fca84917948b642ea87b66bf228b47
Yiteng Dang <yitengd@gmail.com>
2018-05-30 18:10:31 +0200

    2018-05-30 end of day

--- two_signals/time_evolution_multiple_signals_multiply_save.m
+++ two_signals/time_evolution_multiple_signals_multiply_save.m
@@ -1,27 +1,27 @@
 close all
 clear all
+maxNumCompThreads(4);
 %warning off
 %% (1) input parameters
-%{
 % lattice parameters
-gridsize = 15;
-N = gridsize^2;
+gz = 15;
+N = gz^2;
 a0 = 1.5;
 rcell = 0.2;
 Rcell = rcell*a0;
 
 % circuit parameters
-Con = [35 15];
+Con = [18 16];
 Coff = [1 1];
-M_int = [1 -1; 1 0];
-K = [8 25; 35 0]; % K(i,j): sensitivity of type i to type j molecules
+M_int = [1 1; -1 -1];
+K = [3 12; 13 20]; % K(i,j): sensitivity of type i to type j molecules
 lambda = [1 1.2]; % diffusion length (normalize first to 1)
-hill = Inf; 
+hill = Inf;
 noise = 0;
 
 % initial conditions
-p0 = [0.2 0.6];
-iniON = round(p0*N);
+%p0 = [0.2 0.6];
+%iniON = round(p0*N);
 I0 = [0 0];
 dI = 0.01;
 InitiateI = 0; % 0: no, 1: yes
@@ -30,14 +30,20 @@ InitiateI = 0; % 0: no, 1: yes
 cell_type = zeros(N,1);
 
 % simulation parameters
-tmax = 100;
+tmax = 10000;
 nruns = 10;
 
+% pos, dist
+[dist, pos] = init_dist_hex(gz, gz);
+%[pos,ex,ey] = init_cellpos_hex(gridsize,gridsize);
+%dist = dist_mat(pos,gridsize,gridsize,ex,ey);
+
+%{
 fname_str = strrep(sprintf('N%d_iniON_%d_%d_M_int_%d_%d_%d_%d_a0_%.1f_Con_%d_%d_K_%d_%d_%d_%d_lambda_%.1f_%.1f', ...
     N, iniON(1), iniON(2), M_int(1,1), M_int(1,2), M_int(2,1), M_int(2,2), ...
     a0, Con(1), Con(2), K(1,1), K(1,2), K(2,1), K(2,2),...
     lambda(1), lambda(2)), '.', 'p');
-
+%}
 % check parameters
 idx = (M_int == 0);
 if ~all(K(idx)==0)
@@ -45,10 +51,6 @@ if ~all(K(idx)==0)
     warning('K has wrong entries!');
 end
 
-% pos, dist
-[dist, pos] = init_dist_hex(gz, gz);
-%[pos,ex,ey] = init_cellpos_hex(gridsize,gridsize);
-%dist = dist_mat(pos,gridsize,gridsize,ex,ey);
 %}
 %{
 % TO DO: vectorize
@@ -63,12 +65,13 @@ fprintf('inhibitor fij(a0) = %.4f \n', sinh(Rcell)*sum(exp((Rcell-a0)./lambda(2)
 %}
 
 %% (2) Load parameters from saved trajectory
+%{
 % with parameters saved as structure array 
 % load data
 data_folder = 'H:\My Documents\Multicellular automaton\app\Multicellularity-2.1\data\time_evolution';
 [file, path] = uigetfile(fullfile(data_folder, '\*.mat'), 'Load saved simulation');
 load(fullfile(path, file));
-%%
+
 s = save_consts_struct;
 N = s.N;
 a0 = s.a0;
@@ -90,7 +93,7 @@ Rcell = rcell*a0;
 % simulation parameters
 %tmax = 100;
 tmax = 10^4;
-nruns = 1;
+nruns = 10;
 cell_type = zeros(N,1);
  
 % Initial I
@@ -116,15 +119,53 @@ end
 %}
 
 %% Loop over p_ini
-[p1, p2] = meshgrid(0:0.1:1, 0:0.1:0.5);
-%p1 = 0.5; 
+% -- setup
+[p1, p2] = meshgrid(0:0.1:1, 0:0.1:1);
+%p1 = 0.5;
 %p2 = 0.5;
+% folder to save in
+folder = fullfile('H:\My Documents\Multicellular automaton\data\two_signals\time_evolution',...
+    'III_chaotic_scan_p_ini_batch2');
+
+% default file name
+sim_ID = 'two_signal_mult';
+M_int_str = sprintf('M_int%d_%d_%d_%d', M_int(1,1), M_int(1,2), M_int(2,1), M_int(2,2));
+K_str = sprintf('K%.1f_%.1f_%.1f_%.1f', K(1,1), K(1,2), K(2,1), K(2,2));
+Con_str = sprintf('Con%.1f_%.1f', Con(1), Con(2));
+Coff_str = sprintf('Coff%.1f_%.1f', Coff(1), Coff(2));
+I_ini_str = '';
+if InitiateI
+    I_ini_str = sprintf('_I1_I2_%.2f_%.2f', I0(1), I0(2));
+end
+lambda12 = lambda(2);
+
+    
+%% -- loop
 for i1 = 1:size(p1, 1)
     for i2 = 1:size(p1, 2)
         fprintf('p1 = %.1f, p2 = %.1f \n', p1(i1, i2),  p2(i1, i2) )
         p0 = [ p1(i1, i2)  p2(i1, i2) ];
+        
+        % default file name
+        p_ini_str = sprintf('p_ini%.2f_%.2f', p0(1), p0(2));
+        fname_str = strrep(sprintf('%s_%s_hill%.2f_N%d_a0_%.2f_%s_%s_%s_noise%.1f_%s%s_l12_%.1f_rcell%.1f_tmax%d',...
+            sim_ID, M_int_str, hill, N, a0, K_str, Con_str, Coff_str, noise,...
+            p_ini_str, I_ini_str, lambda12, rcell, tmax), '.', 'p');
+        
+        % skip parameters that are complete
+        if exist(fullfile(folder, sprintf('%s-v%d.mat', fname_str, nruns)), 'file')==2
+           disp('parameter set done, continued')
+           continue
+        end        
+        
         % Dynamics
         for run=1:nruns
+            % skip runs that are complete
+            if exist(fullfile(folder, sprintf('%s-v%d.mat', fname_str, run)), 'file')==2
+               fprintf('run %d exists, continued \n', run);
+               continue
+            end
+            
             disp(run);
             cells_hist = {};
             %t_out = 0;
@@ -166,9 +207,6 @@ for i1 = 1:size(p1, 1)
                 warning('No simulation data to save!');
             end
 
-            % folder to save in
-            folder = fullfile('H:\My Documents\Multicellular automaton\data\two_signals\time_evolution',...
-                'III_chaotic_scan_p_ini_batch2');
             ext = '.mat';
             label = '';
 
@@ -177,27 +215,15 @@ for i1 = 1:size(p1, 1)
             end
 
             % filename 
-            %SimID = {'one_signal', 'two_signal_mult', 'two_signal_add'};
-            sim_ID = 'two_signal_mult';
-            M_int_str = sprintf('M_int%d_%d_%d_%d', M_int(1,1), M_int(1,2), M_int(2,1), M_int(2,2));
-            K_str = sprintf('K%.1f_%.1f_%.1f_%.1f', K(1,1), K(1,2), K(2,1), K(2,2));
-            Con_str = sprintf('Con%.1f_%.1f', Con(1), Con(2));
-            Coff_str = sprintf('Coff%.1f_%.1f', Coff(1), Coff(2));
-            p_ini_str = sprintf('p_ini%.2f_%.2f', p0(1), p0(2));
-            I_ini_str = '';
-            if InitiateI
-                I_ini_str = sprintf('_I1_I2_%.2f_%.2f', I0(1), I0(2));
-            end
-            lambda12 = lambda(2);
-            filename = strrep(sprintf('%s_%s_hill%.2f_N%d_a0_%.2f_%s_%s_%s_noise%.1f_%s%s_l12_%.1f_rcell%.1f_tmax%d',...
-                sim_ID, M_int_str, hill, N, a0, K_str, Con_str, Coff_str, noise, p_ini_str, I_ini_str, lambda12, rcell, tmax), '.', 'p');
+            %filename = strrep(sprintf('%s_%s_hill%.2f_N%d_a0_%.2f_%s_%s_%s_noise%.1f_%s%s_l12_%.1f_rcell%.1f_tmax%d',...
+            %    sim_ID, M_int_str, hill, N, a0, K_str, Con_str, Coff_str, noise, p_ini_str, I_ini_str, lambda12, rcell, tmax), '.', 'p');
 
             % check if filename already exists
             i=1;
-            fname = fullfile(folder, strcat(filename, '-v', num2str(i), label, ext));
+            fname = fullfile(folder, strcat(fname_str, '-v', num2str(i), label, ext));
             while exist(fname, 'file') == 2
                 i=i+1;
-                fname = fullfile(folder, strcat(filename, '-v', num2str(i), label, ext));
+                fname = fullfile(folder, strcat(fname_str, '-v', num2str(i), label, ext));
             end
 
             save_vars = {N, a0, K, Con, Coff, M_int, hill, noise, p0, I0, rcell,...
