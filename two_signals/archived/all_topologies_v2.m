19c5ace1c0a577cf8861d7e68fa3fb4c42cb65a8
Yiteng Dang <yitengd@gmail.com>
2018-08-05 10:08:53 +0200

    minor mods

--- two_signals/archived/all_topologies_v2.m
+++ two_signals/archived/all_topologies_v2.m
@@ -2,9 +2,7 @@
 clear variables
 close all
 clc
-% NB: phase symmetries seem to be wrong, not working properly
 %% Count # of topologies
-% # phases per interaction
 n_phases_all = [6 2];
 
 % Single cell
@@ -17,42 +15,47 @@ na = [0 8 20 32 12]./2; % remaining
 
 N_phases = sum(Ts.*ns+Ta.*na); % total # phases
 N_phases_all = sum( (ns+2*na).*Ta );
-fprintf('Total # single-cell phases (before symmetries): %d \n', N_phases_all)
-fprintf('Total # single-cell phases (after symmetries): %d \n', N_phases)
+fprintf('Total # single-cell phases (excl. sym. partners): %d \n', N_phases_all)
+fprintf('Total # single-cell phases (incl. sym. partners): %d \n', N_phases)
 
 % multiple cells
 disp('Multiple cells:');
 n_phases = n_phases_all(1);
-Ta = n_phases.^(0:4);
 n = zeros(1,5);
 for k=0:4
     n(k+1) = nchoosek(4,k)*2^k;
 end
-Ns1 = sum(n.*Ta);
+na = n_phases.^(0:4);
+Ns1 = sum(n.*na);
 
 % account for symmetry
-ns2 = [1 0 4 0 4]; % self-similar
-na2 = [0 8 20 32 12]./2; % remaining
-Ts2 = [1 0 21 0 666]; % #states for self-similar topologies
-Ta2 = n_phases.^(0:4);
+Ts = [1 0 4 0 4]; % self-similar
+Ta = [0 8 20 32 12]./2; % remaining
+ns = [1 0 21 0 666]; % #states for self-similar topologies
 %Ns2a = sum(Ta.*na); % states without self-similarity
-Ns2 = sum(Ts2.*ns2+Ta2.*na2);
+Ns2 = sum(Ts.*ns+Ta.*na);
 
 % exclude trivial topologies
-ns3 = [0 0 2 0 4];
-na3 = [0 0 10 32 12]./2; % remaining
-Ns3 = sum(Ts2.*ns3+Ta2.*na3);
+Ts = [0 0 2 0 4];
+Ta = [0 0 10 32 12]./2; % remaining
+Ns3 = sum(Ts.*ns+Ta.*na);
 
 % account for symmetries
-fprintf('Total # multi-cell phases (before symmetries): %d \n', Ns1)
-fprintf('Total # multi-cell phases (after symmetries): %d \n', Ns2)
+fprintf('Total # multi-cell phases (excl. sym. partners): %d \n', Ns1)
+fprintf('Total # multi-cell phases (incl. sym. partners): %d \n', Ns2)
 %fprintf('States without self-similarity: %d \n', Ns2a)
 fprintf('Excluding trivial topologies: %d \n', Ns3)
 
-% Convention:
+% Phase conventions
 % phase 1: (Con - K) > 0
 % phase 2: (Con - K) < 0
 
+% 1: none (activation-deactivation)
+% 2: all ON(+) / OFF(-)
+% 3: A1: ON->ON (+) / ON-> OFF (-)
+% 4: A0: OFF->OFF (+) / OFF->ON (-)
+% 5: all OFF(+) / ON(-)
+% 6: A01: autonomy (+) / autonomous oscillations (-)
 %% Loop over topologies and phases
 % N.B. some of the phases are not possible, e.g. when autonomy and
 % activation-deactivation are both present for the same gene
@@ -60,7 +63,7 @@ fprintf('Excluding trivial topologies: %d \n', Ns3)
 % Settings
 single_cell = 0;
 draw_diagram = 0; % draw state diagram?
-sym = 0; % include symmetries? 0: symmetric diagrams are excluded, 1: everything included
+sym = 1; % include symmetries? 0: symmetric diagrams are excluded, 1: everything included
 n_phases = n_phases_all(single_cell+1);
 
 % tracking variables
@@ -69,7 +72,7 @@ M = [0 1 -1]; % index to interaction
 count = 0; %count topologies
 countP = zeros(3^4, 1); % count phases
 
-% main output
+% main output 
 M_int_all = {}; %cell(3^4, 1);
 phases_all = {}; % store all phases, stored as matrix with phase of each interaction (1-6), 0 if no interaction (M_int(i,j)==0)
 state_diagrams = {}; % cell(Ns2, 1); % store all state diagrams (graph transition matrices)
@@ -101,30 +104,28 @@ for k=1:3^4
         if ~sym
             done(gM(1,1),gM(1,2),gM(2,1),gM(2,2))=1;
         end
-        
         % loop over all phases
         ni = sum(abs(M_int(:))==1);
         sz = n_phases*abs(M_int)+(1-abs(M_int)); % size matrix, for correct index conversion
         doneP = zeros(sz(1,1), sz(1,2), sz(2,1), sz(2,2));
         % distinguish between self-similar topologies and others
         if all(all(g(M_int) == M_int))
-            % topology symmetry 1<->2
+            % special case: topology symmetry 1<->2
             %disp(M_int);
             for k1=1:n_phases^ni
                 [i11b, i12b, i21b, i22b] = ind2sub([sz(1,1), sz(1,2), sz(2,1), sz(2,2)], k1);
                 if doneP(i11b,i12b,i21b,i22b)
                     continue
-                elseif (i11b==1)&&(sz(1,1)>1)&&(i12b==6) || (i11b==6)&&(i12b==1)&&(sz(1,2)>1)
+                elseif (i11b==1)&&(i12b==6) || (i11b==6)&&(i12b==1) 
                     doneP(i11b,i12b,i21b,i22b) = 1;
                     continue
-                elseif (i21b==1)&&(sz(2,1)>1)&&(i22b==6) || (i21b==6)&&(i22b==1)&&(sz(2,2)>1)
+                elseif (i21b==1)&&(i22b==6) || (i21b==6)&&(i22b==1) 
                     doneP(i11b,i12b,i21b,i22b) = 1;
                     continue
                 else
-                    %disp(sum(countP));
+                    % 
+                    disp(sum(countP));
                     P = [i11b i12b; i21b i22b]; % matrix with phases
-                    
-                    %{
                     phases_all{end+1} = abs(M_int).*P; % store phase matrix 
                     M_int_all{end+1} = M_int; %store interaction matrix
                     
@@ -132,7 +133,7 @@ for k=1:3^4
                     state_diagrams{end+1} = A;
                     steady_states{end+1} = ss;
                     cycles_all{end+1} = cycles;
-                    %}
+                    
                     % update tracking variables: also consider P symmetries
                     doneP(i11b,i12b,i21b,i22b) = 1;
                     gP = g(P);
@@ -153,14 +154,14 @@ for k=1:3^4
                 [i11b, i12b, i21b, i22b] = ind2sub([sz(1,1), sz(1,2), sz(2,1), sz(2,2)], k1);
                 if doneP(i11b,i12b,i21b,i22b)
                     continue
-                elseif (i11b==1)&&(sz(1,1)>1)&&(i12b==6) || (i11b==6)&&(i12b==1)&&(sz(1,2)>1)
+                elseif (i11b==1)&&(i12b==6) || (i11b==6)&&(i12b==1) 
                     doneP(i11b,i12b,i21b,i22b) = 1;
                     continue
-                elseif (i21b==1)&&(sz(2,1)>1)&&(i22b==6) || (i21b==6)&&(i22b==1)&&(sz(2,2)>1)
+                elseif (i21b==1)&&(i22b==6) || (i21b==6)&&(i22b==1) 
                     doneP(i11b,i12b,i21b,i22b) = 1;
                     continue
                 else
-                    %{
+                    % 
                     disp(sum(countP));
                     P = [i11b i12b; i21b i22b]; % matrix with phases
                     phases_all{end+1} = abs(M_int).*P; % store phase matrix
@@ -170,7 +171,6 @@ for k=1:3^4
                     state_diagrams{end+1} = A;
                     steady_states{end+1} = ss;
                     cycles_all{end+1} = cycles;
-                    %}
                     
                     % update tracking variables: no need to consider P symmetries
                     doneP(i11b,i12b,i21b,i22b) = 1;
@@ -191,8 +191,8 @@ end
 fprintf('Total # phases considered: %d \n', sum(countP))
 %%
 % save data
-%save_path = 'H:\My Documents\Multicellular automaton\data\two_signals\all_topologies';
-save_path = 'D:\Multicellularity\data\two_signals\all_topologies';
+save_path = 'H:\My Documents\Multicellular automaton\data\two_signals\all_topologies';
+%save_path = 'D:\Multicellularity\data\two_signals\all_topologies';
 labels = {'multi_cell', 'single_cell', 'multi_cell_all_incl', 'single_cell_all_incl'};
 qsave = 1;
 if qsave
@@ -203,14 +203,16 @@ if qsave
 end
 
 %% For given topology (M_int), loop over all phases
-%{
 countP = 0;
 M_int = [0 -1; 1 1];
+subfolder = sprintf('diagrams_M_int_%d_%d_%d_%d', M_int(1,1),...
+    M_int(1,2), M_int(2,1), M_int(2,2));
 
 % Settings
-single_cell = 1;
+single_cell = 0;
 draw_diagram = 1; % draw state diagram?
 save_diagram = 1;
+sym = 1; % Include symmetric partners?
 labels = {'multi_cell', 'single_cell'};
 label = labels{single_cell+1};
 
@@ -231,10 +233,10 @@ for k1=1:n_phases^ni
     [i11b, i12b, i21b, i22b] = ind2sub([sz(1,1), sz(1,2), sz(2,1), sz(2,2)], k1);
     if doneP(i11b,i12b,i21b,i22b)
         continue
-    elseif (i11b==1)&&(i12b==6) || (i11b==6)&&(i12b==1) 
+    elseif (i11b==5)&&(i12b==6) || (i11b==6)&&(i12b==5) 
         doneP(i11b,i12b,i21b,i22b) = 1;
         continue
-    elseif (i21b==1)&&(i22b==6) || (i21b==6)&&(i22b==1) 
+    elseif (i21b==5)&&(i22b==6) || (i21b==6)&&(i22b==5) 
         doneP(i11b,i12b,i21b,i22b) = 1;
         continue    
     else
@@ -247,7 +249,7 @@ for k1=1:n_phases^ni
         
         count = count+1;
         %save_folder = fullfile('H:\My Documents\Multicellular automaton\figures\two_signals\all_topologies\temp');
-        save_folder = 'H:\My Documents\Multicellular automaton\figures\two_signals\all_topologies\single_cell_by_topology';
+        save_folder = fullfile('D:\Multicellularity\figures\two_signals\all_topologies', subfolder);
         phase = abs(M_int).*P;
         fname_str = sprintf('state_diagram_%s_phase_%d_%d_%d_%d', label,...
             phase(1,1), phase(1,2), phase(2,1), phase(2,2));
@@ -256,15 +258,17 @@ for k1=1:n_phases^ni
             
         % update tracking variables: also consider P symmetries
         doneP(i11b,i12b,i21b,i22b) = 1;
-        %gP = g(P);
-        %doneP(gP(1,1),gP(1,2),gP(2,1),gP(2,2))=1;
+        gP = g(P);
+        if ~sym
+            doneP(gP(1,1),gP(1,2),gP(2,1),gP(2,2))=1;
+        end
         countP = countP + 1;
         pause(1);
         close all
     end
 end
 fprintf('Total # phases considered: %d \n', sum(countP))
-%}
+
 %% Functions
 
 % symmetry operation 1<->2, finds the topology that is equivalent up to
